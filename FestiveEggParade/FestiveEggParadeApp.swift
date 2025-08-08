9ea151bd128bde6f5bd285c7bd8aec18ce43fb54
AezakmiEvgen <evgenii.aezakmi@gmail.com>
2025-08-08 19:06:23 +0300

    Initial Commit

---
+++ FestiveEggParade/FestiveEggParadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestiveEggParadeApp.swift
+//  FestiveEggParade
+//
+//  Created by Евгений on 08.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct FestiveEggParadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
